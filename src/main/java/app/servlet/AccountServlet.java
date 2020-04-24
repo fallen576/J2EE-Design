76fbb6e6102d2fe7cb4eb2a769f048345b34ddaa
Kevin Gerstein <Kg1941a@student.american.edu>
2020-04-24 14:29:59 -0400

    add user to DB when they create account

--- src/main/java/app/servlet/AccountServlet.java
+++ src/main/java/app/servlet/AccountServlet.java
@@ -1,12 +1,22 @@
 package app.servlet;
 
 import java.io.IOException;
+import java.sql.Connection;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import app.dao.user.SqlUserDao;
+import app.dao.user.UserDao;
+import app.model.User;
+import app.services.user.DefaultUserService;
+import app.services.user.UserService;
 //import javax.servlet.http.HttpSession;
+import app.utils.ServletUtils;
 
 /**
  * Servlet implementation class AccountServlet
@@ -14,34 +24,53 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/account")
 public class AccountServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AccountServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	
+	private UserService userService;
+	private UserDao userDao;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public AccountServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
+	public void init() throws ServletException {
+		Connection connection = ServletUtils.connectToSqlDatabase();
+		
+		userDao = new SqlUserDao(connection);
+		userService = new DefaultUserService(userDao);
+	}
+	
+	
+
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//HttpSession session = request.getSession();
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		 HttpSession session = request.getSession();
+
 		String createAccount = (String) request.getParameter("createAccount");
-		
-		//User is looking to create a new account
-		if(createAccount.equals("true")) {
+
+		// User is looking to create a new account
+		if (createAccount.equals("true")) {
 			response.sendRedirect(request.getContextPath() + "/createAccount.jsp");
-		} else if(createAccount.equals("done")) {
+		} else if (createAccount.equals("done")) { 
+			//user created account, now store info in DB and set user session.
+			
+			String firstName = (String) request.getParameter("firstName");
+			String lastName = (String) request.getParameter("lastName");
+			String password = (String) request.getParameter("password");
+			String email = (String) request.getParameter("email");
+			
+			User user = new User(firstName, lastName, password, email);
+			session.setAttribute("user", user);
+			userService.insert(firstName, lastName, password, email);
+			
+			
 			response.sendRedirect(request.getContextPath() + "/index.jsp");
 		}
 	}
